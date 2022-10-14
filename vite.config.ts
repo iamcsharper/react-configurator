53e81a6121f934df42ef6a1016743fc731b4352a
Ilya Yurchenko <ilyha19@live.ru>
2022-10-14 13:10:23 +0300

    fix env

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ const ASSET_URL = process.env.ASSET_URL || '';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    EnvironmentPlugin(['ASSET_URL', 'BASE_URL']),
+    EnvironmentPlugin(['ASSET_URL', 'BASE_URL', 'USE_LOCAL_STORAGE']),
     svgr(),
     tsconfigPaths(),
     react({
